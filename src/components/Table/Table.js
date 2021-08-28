8ed43ce66600c6ea5d9e1197f5fcd4978409e287
jdpelletier <john.pelletier@ymail.com>
2021-08-28 03:55:22 -1000

    moving name isolation to same line as date

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -57,69 +57,69 @@ export const Table = ({dat, cols, getCellProps, hiddenColumns=[]}) => {
     }
   },
   useFilters)
-
+  // TODO clean up &nbsp;
   return (
     <>
-    <div>
-      Current schedule start date: {format(new Date(data[0].Date), 'MM/dd/yyy')} End date: {format(new Date(data[data.length -1].Date), 'MM/dd/yyy')}
-    </div>
-      <div className="bb b--white">
-        <br/>
-        <label>Show schedule for:</label>
-        <select name="names" id="names" onChange={(e) => {
-            const selection = document.getElementById("names").options[document.getElementById("names").selectedIndex].value
-            if (selection === "All") {
-              toggleAllCols()
-            }else{
-              isolateColumn(allColumns, selection, e)
-            }
-          }}>
-          <option value="All">All</option>
-          {allColumns.filter(column => column.id.length < 4 && column.id !== 'DOW' && column.id !== 'MTG').map(column => {
-            return(<option value={column.id}>{column.id}</option>)
-          })}
-        </select>
-      </div>
-      <div className="tablewrap">
-        <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map( (column) => (
-                  <th {...column.getHeaderProps()}><div className="checkmark"><input type='checkbox' {...column.getToggleHiddenProps()} /></div><br></br>{column.render('Header')}
-                    <div>{column.canFilter ? column.render('Filter') : null}</div>
-                  </th>))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map(row=> {
-              prepareRow(row)
-              return (
-                <tr className={row.original.DOW} {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return <td {...cell.getCellProps([getCellProps(cell)])} onClick={(e) => {
-                      if (cell.column.Header==="Date") {
-                        telSchedule(row.original.Date, e)
-                      }
-                    }}>
-                    {cell.render('Cell')}</td>
-                  })}
-                </tr>
-              )
-              })}
-          </tbody>
-          <tfoot>
-            {footerGroups.map((footerGroup) => (
-                <tr {...footerGroup.getFooterGroupProps()}>
-                  {footerGroup.headers.map(column => (
-                      <td {...column.getFooterProps}>{column.render('Footer')}</td>
-                    ))}
+        <div className="bb b--white">
+          Current schedule start date: {format(new Date(data[0].Date), 'MM/dd/yyy')} End date: {format(new Date(data[data.length -1].Date), 'MM/dd/yyy')}
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          Show schedule for:
+          <select name="names" id="names" onChange={(e) => {
+              const selection = document.getElementById("names").options[document.getElementById("names").selectedIndex].value
+              if (selection === "All") {
+                toggleAllCols()
+              }else{
+                isolateColumn(allColumns, selection, e)
+              }
+            }}>
+            <option value="All">All</option>
+            {allColumns.filter(column => column.id.length < 4 && column.id !== 'DOW' && column.id !== 'MTG').map(column => {
+              return(<option value={column.id}>{column.id}</option>)
+            })}
+          </select>
+        </div>
+        <div className="tablewrap">
+          <table {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map( (column) => (
+                    <th {...column.getHeaderProps()}><div className="checkmark"><input type='checkbox' {...column.getToggleHiddenProps()} /></div><br></br>{column.render('Header')}
+                      <div>{column.canFilter ? column.render('Filter') : null}</div>
+                    </th>))}
                 </tr>
               ))}
-          </tfoot>
-        </table>
-      </div>
-  </>
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map(row=> {
+                prepareRow(row)
+                return (
+                  <tr className={row.original.DOW} {...row.getRowProps()}>
+                    {row.cells.map((cell) => {
+                      return <td {...cell.getCellProps([getCellProps(cell)])} onClick={(e) => {
+                        if (cell.column.Header==="Date") {
+                          telSchedule(row.original.Date, e)
+                        }
+                      }}>
+                      {cell.render('Cell')}</td>
+                    })}
+                  </tr>
+                )
+                })}
+            </tbody>
+            <tfoot>
+              {footerGroups.map((footerGroup) => (
+                  <tr {...footerGroup.getFooterGroupProps()}>
+                    {footerGroup.headers.map(column => (
+                        <td {...column.getFooterProps}>{column.render('Footer')}</td>
+                      ))}
+                  </tr>
+                ))}
+            </tfoot>
+          </table>
+        </div>
+      </>
   )
 }
