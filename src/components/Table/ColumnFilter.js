d4492498f15f4f668f19e3ea9fd2e89a52ffa31e
jdpelletier <john.pelletier@ymail.com>
2023-06-22 17:51:54 -1000

    Update ColumnFilter.js

--- src/components/Table/ColumnFilter.js
+++ src/components/Table/ColumnFilter.js
@@ -2,23 +2,40 @@ import React, { useState } from 'react'
 import { useAsyncDebounce } from 'react-table'
 
 export const ColumnFilter = ({ column }) => {
+  
   const {filterValue, setFilter} = column
-
   const [value, setValue] = useState(filterValue)
+  const [searching, setSearching] = useState(false)
 
   const onChange = useAsyncDebounce(value => {
     setFilter(value || undefined)
   }, 300)
 
-  const input = column.Header.length - 1
+  let width = window.innerWidth;
+  let inSize;
+  let output;
+
+  if(width>750){
+    inSize = "4"
+  }else{
+    inSize="1"
+  }
+
+  if(searching){
+    output = <input autofocus size={inSize} value={value || ''} onChange={(e) => {
+              setValue(e.target.value)
+              onChange(e.target.value)
+            }}/>
+
+  }else{
+    output = <button onClick={() => setSearching(true)}>&#x1F50D;</button>
+  }
 
   return (
     <span>
-      <input size={input.toString()} value={value || ''} onChange={(e) => {
-        setValue(e.target.value)
-        onChange(e.target.value)
-      }}
-    />
+      <div className="searchinput">
+        {output}
+      </div>
     </span>
   )
 }
