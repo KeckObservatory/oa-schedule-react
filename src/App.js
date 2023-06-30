5367723fdeeec71e6350923fe896bc1a6c014744
jdpelletier <john.pelletier@ymail.com>
2023-06-29 17:02:13 -1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -115,18 +115,19 @@ function App () {
           }else{
             setLastDay(data)
           }
-        });
-        fetch("https://vm-www3build:53872/nightstaff", {
-          method: 'post',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({'Start': firstDay, 'End': lastDay })
-        })
-        .then(response => response.json())
-        .then(data => {
-          setSchedule([...data])
-          setColumns([...cols(data)])
-          setObsReady(true)
-        });
+        }).then(
+          fetch("https://vm-www3build:53872/nightstaff", {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({'Start': firstDay, 'End': lastDay })
+          })
+          .then(response => response.json())
+          .then(data => {
+            setSchedule([...data])
+            setColumns([...cols(data)])
+            setObsReady(true)
+          })
+        );
     }else{
       fetch("https://vm-www3build:53872/")
       .then(response => response.json())
