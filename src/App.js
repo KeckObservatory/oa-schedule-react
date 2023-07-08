e9962ffe2abffebf2f730623dd8cc3ff34470c90
jdpelletier <john.pelletier@ymail.com>
2023-07-08 00:00:00 -1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App () {
         const newsched = data.concat(schedule)
         fetch("https://vm-www3build:53872/observers", {
               method: 'post',
-              headers: { 'Content-Type': 'application/json' }
+              headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify({'Schedule': newsched, 'Start': new Date(range[0]).getTime(), 'End': new Date(range[1]).getTime()})
             })
               .then(response => response.json())
