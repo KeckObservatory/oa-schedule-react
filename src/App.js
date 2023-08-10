739d86329a917632c969586440478e76c5e62ca5
jdpelletier <john.pelletier@ymail.com>
2023-08-10 09:55:17 -1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,8 +24,7 @@ function App () {
   const [startDate, endDate] = dateRange;
   const dataFetchedRef = useRef(false);
 
-  // const api ="/api/oa-schedule";
-  const api = "https://vm-oas:53872"
+  const api ="/api/oa-schedule";
 
   const filterRange = (range) => {
     if (new Date(range[0]).getTime() < firstDay && new Date(range[1]).getTime() < firstDay && range[1] !== null){
